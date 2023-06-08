be705608650f4c0b77f78be7613a6a614ea5694e
toska27 <nemanjatoskic58@gmail.com>
2023-06-08 21:48:36 +0200

    Join nastavak predavanja

--- 21_BAZE_RELACIJE/fakultet.sql
+++ 21_BAZE_RELACIJE/fakultet.sql
@@ -48,5 +48,54 @@ ADD CONSTRAINT `nastavnik_ispit_fk`
 FOREIGN KEY(`nastavnik_id`)
 REFERENCES `nastavnici`(`id`);
 
+CREATE TABLE `zvanje`(
+    `id` INT PRIMARY KEY,
+    `naziv_zvanja` VARCHAR(45) NOT NULL
+) ENGINE=INNODB;
 
+CREATE TABLE `smer`(
+    `id` INT PRIMARY KEY,
+    `naziv_smera` VARCHAR(45) NOT NULL
+) ENGINE=INNODB;
 
+CREATE TABLE `katedra`(
+    `id` INT PRIMARY KEY,
+    `naziv_katedre` VARCHAR(45) NOT NULL
+) ENGINE=INNODB;
+
+ALTER TABLE `nastavnici`
+ADD `zvanje_id` INT NOT NULL;
+
+ALTER TABLE `nastavnici`
+MODIFY COLUMN `zvanje_id` INT;
+
+ALTER TABLE `nastavnici`
+ADD CONSTRAINT `nastavnik_zva_fk`
+FOREIGN KEY(`zvanje_id`)
+REFERENCES `zvanje`(`id`);
+
+ALTER TABLE `studenti`
+ADD `smer_id` INT NOT NULL;
+
+ALTER TABLE `studenti`
+MODIFY COLUMN `smer_id` INT;
+
+ALTER TABLE `studenti`
+ADD CONSTRAINT `studenti_sm_fk`
+FOREIGN KEY(`smer_id`)
+REFERENCES `smer`(`id`);
+
+CREATE TABLE `katedra_has_nastavnici`(
+    `nastavnik_id` INT NOT NULL,
+    `katedra_id` INT NOT NULL
+) ENGINE=INNODB;
+
+ALTER TABLE `katedra_has_nastavnici`
+ADD CONSTRAINT `katedra_nastavnici_nastavnik_fk`
+FOREIGN KEY(`nastavnik_id`)
+REFERENCES `nastavnici`(`id`);
+
+ALTER TABLE `katedra_has_nastavnici`
+ADD CONSTRAINT `katedra_nastavnici_katedra_fk`
+FOREIGN KEY(`katedra_id`)
+REFERENCES `katedra`(`id`);
--- 21_BAZE_RELACIJE/join.sql
+++ 21_BAZE_RELACIJE/join.sql
@@ -23,19 +23,7 @@ LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
 WHERE `ispiti`.`datum`='2023-05-15'
 ;
 
-
-SELECT CONCAT(`studenti`.`ime`, ' ', `studenti`.`prezime`) AS `student` ,
-`predmeti`.`naziv`,
-CONCAT(`nastavnici`.`ime`, ' ', `nastavnici`.`prezime`) AS `nastavnik`,
-`ispiti`.`datum`,
-`ispiti`.`ocena`
-FROM `ispiti`
-LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
-LEFT JOIN `predmeti` ON `ispiti`.`predmet_id`=`predmeti`.`id`
-LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
-WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic';
-
-
+-- ZA DATO IME I PREZIME ISPISATI ISPITE KOJE JE POLAGAO
 SELECT CONCAT(`studenti`.`ime`, ' ', `studenti`.`prezime`) AS `student` ,
 `predmeti`.`naziv`,
 CONCAT(`nastavnici`.`ime`, ' ', `nastavnici`.`prezime`) AS `nastavnik`,
@@ -48,4 +36,75 @@ LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
 WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic'
 AND `ispiti`.`ocena`>8
 ;
+-- OVAKO PRAVILNIJE
+SELECT DISTINCT
+`predmeti`.`naziv`
+FROM `ispiti`
+LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
+LEFT JOIN `predmeti` ON `ispiti`.`predmet_id`=`predmeti`.`id`
+LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
+WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic';
 
+-- KAO PROSLI SAMO OCENA VECA OD 8
+SELECT CONCAT(`studenti`.`ime`, ' ', `studenti`.`prezime`) AS `student` ,
+`predmeti`.`naziv`,
+CONCAT(`nastavnici`.`ime`, ' ', `nastavnici`.`prezime`) AS `nastavnik`,
+`ispiti`.`datum`,
+`ispiti`.`ocena`
+FROM `ispiti`
+LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
+LEFT JOIN `predmeti` ON `ispiti`.`predmet_id`=`predmeti`.`id`
+LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
+WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic'
+AND `ispiti`.`ocena`>8
+;
+-- OVAKO PRAVILNIJE
+SELECT DISTINCT
+`predmeti`.`naziv`
+FROM `ispiti`
+LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
+LEFT JOIN `predmeti` ON `ispiti`.`predmet_id`=`predmeti`.`id`
+LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
+WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic' AND `ispiti`.`ocena`>8;
+
+-- Za dato ime i prezime, odredite njegovu prosecnu ocenu
+SELECT AVG(`ispiti`.`ocena`) AS `srednja_ocena` 
+FROM `ispiti`
+LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
+WHERE `studenti`.`ime`='Nemanja' AND `studenti`.`prezime`='Toskic' 
+AND `ispiti`.`ocena`>5;
+
+-- Za dat naziv predmeta odrediti maksimalnu ocenu na nekom ispitu iz tog predmeta.
+SELECT MAX(`ispiti`.`ocena`) AS `max_ocena` 
+FROM `ispiti`
+LEFT JOIN `predmeti` ON `ispiti`.`predmet_id`=`predmeti`.`id`
+WHERE `predmeti`.`naziv`='CSS'
+AND `ispiti`.`datum` = '2023-04-17';
+
+-- Za dat datum i nastavnika odrediti prosečnu ocenu svih ispita koji su se polagali tog dana a koje je ocenio taj nastavnik.
+SELECT AVG(`ispiti`.`ocena`) as `prosecna_ocena` FROM `ispiti`
+LEFT JOIN `nastavnici` ON `ispiti`.`nastavnik_id`=`nastavnici`.`id`
+WHERE `ispiti`.`datum` = '2023-04-17'
+AND `nastavnici`.`ime`='Stefan'
+AND `ispiti`.`ocena`>5;
+
+SELECT * FROM `ispiti` as `i`
+LEFT JOIN `nastavnici` as `n` ON `i`.`nastavnik_id`=`n`.`id`  -- moze i ovako
+
+-- Za dati datum ispisati imena i prezimena studenata koji nisu polagali ispit tog dana
+
+-- studenti koji su polagali taj dan
+SELECT CONCAT(`studenti`.`ime`, ' ', `studenti`.`prezime`) AS `student` 
+FROM `ispiti`
+LEFT JOIN `studenti` ON `ispiti`.`student_indeks`=`studenti`.`indeks`
+WHERE `ispiti`.`datum`='2023-04-18';
+
+-- studenti koji nisu polagali taj dan
+SELECT * FROM `studenti`
+WHERE `studenti`.`indeks` NOT IN (SELECT `ispiti`.`student_indeks` from `ispiti` WHERE `ispiti`.`datum` = '2023-04-18');
+
+-- studenti koji nisu polagali taj dan (moze i ovako)
+SELECT CONCAT(`studenti`.`ime`, ' ', `studenti`.`prezime`) AS `student` 
+FROM `studenti`
+LEFT JOIN `ispiti` ON `ispiti`.`student_indeks`=`studenti`.`indeks` AND `ispiti`.`datum` = '2023-04-18'
+WHERE `ispiti`.`id` is null;
